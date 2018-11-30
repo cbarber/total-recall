06a30dc089680c5a432894b1a10014fb3c0e7d0d
Craig Barber <craigb@mojotech.com>
2018-11-30 15:34:07 -0500

    feature: handle zsh and bash shell histories

--- lib/total_recall/shell_history.rb
+++ lib/total_recall/shell_history.rb
@@ -1,8 +1,9 @@
 module TotalRecall
   # Shell history enumerable
   class ShellHistory
-    def initialize
-      @shell = ENV['SHELL']
+    def initialize(shell = nil)
+      @shell = shell || File.basename(ENV['SHELL'])
+      @histfile = ENV['HISTFILE'] || histfile_path
     end
 
     def each(&block)
@@ -11,7 +12,33 @@ module TotalRecall
 
     protected def history
       Logging.logger.info("Fetching history for #{@shell}")
-      `$SHELL -c history`.split(/\r?\n/)
+      case @shell
+      when 'bash', 'dash', 'zsh'
+        return numbered_history_from_histfile
+      else
+        return history_from_builtin
+      end
+    end
+
+    protected def history_from_builtin
+      Logging.logger.info('Fetching history from builtin')
+      `#{@shell} -c history`.split(/\r?\n/)
+    end
+
+    protected def numbered_history_from_histfile
+      Logging.logger.info("Fetching history from #{@histfile}")
+      File.readlines(@histfile).map do |line|
+        line.sub(/^\s+[0-9]+\s+/, '')
+      end
+    end
+
+    protected def histfile_path
+      case @shell
+      when 'bash', 'dash'
+        return "#{ENV['HOME']}/.bash_history"
+      when 'zsh'
+        "#{ENV['HOME']}/.zsh_history"
+      end
     end
   end
 end
